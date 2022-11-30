dc89ac0a7cc58c28ab84f9f5092d39967c664d4b
Artem Panchenko <Ymrtpnch@gmail.com>
2022-11-30 21:31:33 +0600

    +buttontest

--- AlchemyCart/ViewController.swift
+++ AlchemyCart/ViewController.swift
@@ -9,12 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LabelToHi: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func HiLabel(_ sender: UIButton) {
+        LabelToHi.text = "123"
+    }
+    @IBAction func ClearLabel(_ sender: Any) {
+        LabelToHi.text = ""
+    }
+    
 }
-//test git
-//test?
